0c5e0665817cfe8a6eb2e93cf4c57d22a4861876
Edmond Chuc <edmond@kurrawong.ai>
2025-02-07 13:25:42 +1000

    Add updated data

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,8 +4,16 @@
 import ai.kurrawong.jena.compoundnaming.GetComponentsPropertyFunctionFactory
 
 import org.apache.jena.query.*
+import org.apache.jena.rdf.model.Model
 import org.apache.jena.rdf.model.ModelFactory
 import org.apache.jena.sparql.pfunction.PropertyFunctionRegistry
+
+fun loadModelFromResource(resourceName: String): Model {
+    val stream = ClassLoader.getSystemResourceAsStream(resourceName)
+    val model = ModelFactory.createDefaultModel()
+    model.read(stream, null, "TURTLE")
+    return model
+}
 
 fun main() {
     val propertyFunctionRegistry = PropertyFunctionRegistry.chooseRegistry(ARQ.getContext())
@@ -15,22 +23,11 @@ fun main() {
     )
     PropertyFunctionRegistry.set(ARQ.getContext(), propertyFunctionRegistry)
 
-    val reader = object {}.javaClass.getResourceAsStream("data.ttl")?.bufferedReader()
-    val reader2 = object {}.javaClass.getResourceAsStream("data2.ttl")?.bufferedReader()
-
     val dataset = DatasetFactory.createTxnMem()
-    val model = ModelFactory.createDefaultModel()
-    model.read(reader, "https://example.com/", "TURTLE")
+    val model = loadModelFromResource("data.ttl")
+    val model2 = loadModelFromResource("data2.ttl")
     dataset.addNamedModel("urn:graph:address", model)
-
-    val model2 = ModelFactory.createDefaultModel().read(
-        "https://cdn.jsdelivr.net/gh/kurrawong/exem-ont@bf22aa548c635f43f221df7362d8ddc2d99edcc7/exem.ttl",
-        "TURTLE"
-    )
-    dataset.addNamedModel("urn:graph:other", model2)
-
-    val model3 = dataset.defaultModel
-    model3.read(reader2, "https://example.com/", "TURTLE")
+    dataset.addNamedModel("urn:graph:address2", model2)
 
     val queryString = """
 PREFIX func: <https://linked.data.gov.au/def/cn/func/>
@@ -39,8 +36,8 @@ PREFIX sdo: <https://schema.org/>
 SELECT *
 WHERE {
     GRAPH ?g {
-        BIND(<https://linked.data.gov.au/dataset/qld-addr/addr-obj-1837741> AS ?objectIri)
-        ?objectIri sdo:name ?iri .
+        # BIND(<https://linked.data.gov.au/dataset/qld-addr/addr-2522774> AS ?iri)
+        BIND(<https://linked.data.gov.au/dataset/qld-addr/address/e37309a2-3916-506e-b334-30ebb444c213> AS ?iri)
         ?iri <java:ai.kurrawong.jena.compoundnaming.getComponents> (?componentId ?componentType ?componentValuePredicate ?componentValue) .
     }
 }
