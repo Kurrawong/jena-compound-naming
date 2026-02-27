0118c45b7d9e51e5450b62c70904f51940d158d4
Edmond Chuc <edmond@kurrawong.ai>
2026-02-27 16:21:45 +1000

    feat: upgrade to jena 6.0.0 with temporary fuseki pr11 base

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 
 exec \
   "${JAVA_HOME}/bin/java" \
-  ${JAVA_OPTS} \
+  ${JAVA_OPTIONS:-${JAVA_OPTS:-}} \
   -Xshare:off \
   -Dlog4j.configurationFile="${FUSEKI_HOME}/log4j2.properties" \
   -cp "${FUSEKI_HOME}/fuseki-server.jar:${FUSEKI_HOME}/lib/*" \
